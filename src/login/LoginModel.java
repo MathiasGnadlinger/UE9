e7f13cc63e84bc1fd0a59147b1b8564849cc8ded
Mathias Gnadlinger <mathias.gnadlinger@htl-wels.at>
2020-12-08 16:41:42 +0100

    LoginModel

--- src/login/LoginModel.java
+++ src/login/LoginModel.java
@@ -1,4 +1,25 @@
 package login;
 
-public class LoginModel {
+public class LoginModel
+{
+    String username;
+    String password;
+
+    public LoginModel()
+    {
+    this.username = "cisco";
+    this.password = "cisco";
+    }
+
+    public boolean iscorrect (String name, String password)
+    {
+        if(name.equals(username) && this.password.equals(password))
+        {
+           return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
