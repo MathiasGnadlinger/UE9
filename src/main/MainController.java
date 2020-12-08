9d5673ec9dc138e821eddd4444f71e523824290d
Mathias Gnadlinger <mathias.gnadlinger@htl-wels.at>
2020-12-08 19:07:41 +0100

    MainController

--- src/main/MainController.java
+++ src/main/MainController.java
@@ -26,5 +26,44 @@ public class MainController implements Initializable
     @FXML
     private Button b_ask;
 
-    
+    private Stage stage;
+
+    private MainModel model;
+
+    public void setStage(Stage stage)
+    {
+        this.stage = stage;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        model = new MainModel();
+    }
+
+    public static void show(Stage stage)
+    {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainController.class.getResource("main.fxml"));
+            Parent root = fxmlLoader.load();
+
+            //get controller which is connected to this fxml file
+            MainController ctrl = fxmlLoader.getController();
+            ctrl.setStage(stage);
+
+            stage.setTitle("Magic 8 Ball");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Something wrong with welcome.fxml!");
+            ex.printStackTrace(System.err);
+            System.exit(1);
+        }
+    }
+    public void text()
+    {
+        tf_answer.setText(model.getAnswer(tf_question.getText()));
+    }
+
 }
